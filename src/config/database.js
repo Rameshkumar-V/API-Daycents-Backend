c4ea566cc6920e17dd54db193c7f13ee5ed7a462
Rameshkumar-V <vrameshkumar@60@gmail.com>
2025-05-01 20:51:04 +0530

    d

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
 
-console.log(process.env); // This will show all loaded environment variables
+// console.log(process.env); // This will show all loaded environment variables
 
-console.log(process.env.DATABASE_URL)
+// console.log(process.env.DATABASE_URL)
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
