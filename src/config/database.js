72127788c6eda801e30184ff9c90471beca07505
Rameshkumar-V <vrameshkumar@60@gmail.com>
2025-05-02 07:13:44 +0530

    Dialect module added

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-
+const pg = require('pg');
 
 // console.log(process.env); // This will show all loaded environment variables
 
@@ -8,6 +8,7 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
+  dialectModule: pg,
   dialectOptions: {
     ssl: {
       rejectUnauthorized: false
